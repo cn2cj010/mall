a0a5d60ea2a8184db595257c4d373b1ba2905cf4
Jimmy Chang <Jimmy@MACdeMacBook-Pro.local>
2020-12-07 14:48:12 +0800

    first commit

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="isActive">
+      <slot name="item-icon-active"></slot>
+    </div>
+
+    <div v-else>
+      <slot name="item-icon"></slot>
+    </div>
+
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    link: String,
+    activeColor: {
+      type: String,
+      default: "blue",
+    },
+  },
+
+  computed: {
+    isActive() {
+      console.log(this.$route);
+      return this.$route.path.indexOf(this.link) != -1;
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {};
+    },
+  },
+  methods: {
+    itemClick() {
+      this.$router.push(this.link);
+    },
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
---
+++ src/views/category/Category.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: "Category",
+    components: {
+
+    },
+    computed: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
